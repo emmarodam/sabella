62a716433621c22b3aacd3489b0ea2060c6ffbd2
signeth2 <signeth2@gmail.com>
2021-11-25 11:11:10 +0100

    .

--- sabella_keramik/index.php
+++ sabella_keramik/index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php echo get_template_directory_uri(); ?>
+    <link href="<?php echo get_template_directory_uri()?>/css/style.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
