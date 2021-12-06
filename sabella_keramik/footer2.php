733d9cd0084c151940c461135196a7b2bdeb8b96
emmarodam <emmarodam@gmail.com>
2021-12-06 10:47:29 +0100

    footer

---
+++ sabella_keramik/footer2.php
@@ -0,0 +1,34 @@
+<footer>
+  <div class="footer">
+    <section class="footer1">
+        <h2>Menu</h2>
+        <ul>
+          <li>Forside</li>
+          <li>Shop</li>
+          <li>Om mig</li>
+          <li>Kontakt</li>
+        </ul>
+    </section>
+
+    <section class="footer2">
+        <h2>Kundeservice</h2>
+        <ul>
+          <li>Handelsbetingelser</li>
+          <li>Persondatapolitik</li>
+        </ul>
+    </section>
+
+    <section class="footer3">
+        <h2>Følg mig</h2> <br class="br-mobile">
+        <img src="images/facebook.png" width="30px" height="30px"><h3>Denfynsketrædjer/facebook.dk</h3> <br> <br>
+        <img src="images/instagram.png" width="30px" height="30px"><h3>Denfynsketrædjer/instagram</h3>
+    </section>
+    <p style="text-align:center;"> © Copyright | Sabella Keramik 2021</p>
+  </div>
+
+<p> © Copyright | Sabella Keramik 2021</p>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
