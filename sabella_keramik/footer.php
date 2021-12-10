d359914e0204aade38fb3f96934327d288e2df07
signeth2 <signeth2@gmail.com>
2021-12-10 08:45:18 +0100

    rettelser

--- sabella_keramik/footer.php
+++ sabella_keramik/footer.php
@@ -1,35 +1,35 @@
-
-
 <?php wp_footer(); ?>
 <footer>
-  <div class="footer">
-    <section class="footer1">
-        <h2>Menu</h2>
-        <ul>
-          <li>Forside</li>
-          <li>Shop</li>
-          <li>Om mig</li>
-          <li>Kontakt</li>
-        </ul>
-    </section>
+      <div class="footer">
+        <div class="footer1">
+          <h3>Menu</h3>
+            <ul>
+              <li>Forside</li>
+              <li>Shop</li>
+              <li>Om mig</li>
+              <li>Kontakt</li>
+            </ul>
+        </div>
+        <div class="footer2">
+          <h3>Kundeservice</h3>
+            <ul>
+              <li>Handelsbetingelser</li>
+              <li>Persondatapolitik</li>
+            </ul>
+        </div>
+        <div class="footer3">
+          <h3>Følg mig</h3>
+          <ul>
+            <li><a href="https://www.instagram.com/sabella_keramik/"><i class="fab fa-facebook-square some-icons"></i></a>
+        </li>
+            <li><a href="https://www.instagram.com/sabella_keramik/"><i class="fab fa-instagram some-icons"></i></a>
+        </li>
+          </ul>
+        </div>
+      </div>
 
-    <section class="footer2">
-        <h2>Kundeservice</h2>
-        <ul>
-          <li>Handelsbetingelser</li>
-          <li>Persondatapolitik</li>
-        </ul>
-    </section>
-
-    <section class="footer3">
-        <h2>Følg mig</h2> <br class="br-mobile">
-        <img src="images/facebook.png" width="30px" height="30px"><h3>Denfynsketrædjer/facebook.dk</h3> <br> <br>
-        <img src="images/instagram.png" width="30px" height="30px"><h3>Denfynsketrædjer/instagram</h3>
-    </section>
-    <p style="text-align:center;"> © Copyright | Sabella Keramik 2021</p>
-  </div>
-
-</footer>
+      <p id="copyright"> © Copyright | Sabella Keramik 2021</p>
+    </footer>
 
 
 
--- sabella_keramik/front-page.php
+++ sabella_keramik/front-page.php
@@ -1,4 +1,6 @@
 <?php get_header(); ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" type="text/css">
+
 <div class="site-wrapper">
         
         <main>
